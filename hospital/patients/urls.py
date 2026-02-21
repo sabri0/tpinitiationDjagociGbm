89c0292fb2f812f887f14282321ce9e8ddeea790
Sabri Barbaria <sabri.barbaria@impactdev.tn>
2026-02-21 09:41:41 +0100

    sans optimization

---
+++ hospital/patients/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('patients/', views.patient_list, name='patient_list'),
+    path('patients/add/', views.patient_create, name='patient_create'),
+    path('patients/<int:id>/edit/', views.patient_update, name='patient_update'),
+    path('patients/<int:id>/delete/', views.patient_delete, name='patient_delete'),
+]
